1bfe31cf37bedd93dc25ba137d8db98c04d2be20
Malambo <bloxito@gmail.com>
2024-03-20 09:23:38 -0300

    estructura css

---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,9 @@
+
+export default function OrderSummary() {
+
+    return (
+        <aside className='lg:h-screen lg:overflow-y-scroll lg:w-96 md:w-64 p-5'>
+            <h1 className='text-4xl text-center font-black'>Mi pedido</h1>
+        </aside>
+    )
+}
