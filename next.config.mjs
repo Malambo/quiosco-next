d6c70bcf0da585fdd3a3b28bca7baf2d22c628e1
Malambo <bloxito@gmail.com>
2024-05-16 21:39:30 -0300

    Revert "por error en el deploy con Prisma"
    This reverts commit 918c7308b3bdce1c4717466727705a32c256da04.

--- next.config.mjs
+++ next.config.mjs
@@ -7,15 +7,7 @@ const nextConfig = {
                 hostname: "res.cloudinary.com"
             }
         ]
-    },
-    experimental: {
-        serverActions: {
-          allowedOrigins: ["g4ngtx7j-3000.brs.devtunnels.ms/"],
-          allowedForwardedHosts: ["g4ngtx7j-3000.brs.devtunnels.ms/"],
-          
-          // Puedes necesitar usar la propiedad allowedForwardedHosts dependiendo de tu versión exacta.
-        }
-      }
+    }
 };
 
 export default nextConfig;
